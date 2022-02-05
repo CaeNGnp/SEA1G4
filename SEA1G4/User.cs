a603254e8c40d7c4071c99a9a6c5aa6796b2a390
Yuxuan Wong <s10196840@connect.np.edu.sg>
2022-02-05 15:57:41 +0800

    create user class

---
+++ SEA1G4/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEA1G4 {
+    public class User {
+        private string name;
+        private string contactNo;
+        private string emailAddress;
+        private string userId;
+
+        public User(string n, string c, string e, string id) {
+            name = n;
+            contactNo = c;
+            emailAddress = e;
+            userId = id;
+        }
+    }
+}
