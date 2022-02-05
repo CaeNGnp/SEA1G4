d480a5ef465d5a5c7613dcfb13846da99fa754d8
Yuxuan Wong <s10196840@connect.np.edu.sg>
2022-02-05 16:22:31 +0800

    create ridedonestate and payment class

---
+++ SEA1G4/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEA1G4 {
+    public class Payment {
+        //private Ride ride;
+        private double toPay;
+
+        public Payment() { }
+
+        public void applyCreditCard() { }
+        public void applyGiftCard() { }
+        public void applyPoints() { }
+        public void pay() { }
+    }
+}
