9d8bdc569e28150c56d21cd9a32ed56079b587dc
Yuxuan Wong <s10196840@connect.np.edu.sg>
2022-02-07 22:40:27 +0800

    edit creditcard

--- SEA1G4/CreditCard.cs
+++ SEA1G4/CreditCard.cs
@@ -8,12 +8,31 @@ namespace SEA1G4 {
     public class CreditCard {
         private string cardNumber;
         private string accountName;
-        private double amount;
+        private double balance;
 
-        public CreditCard(string no, string name, double amt) {
+        public CreditCard(string no, string name, double bal) {
             cardNumber = no;
             accountName = name;
-            amount = amt;
+            balance = bal;
+        }
+
+        public void deduct(double a) {
+            if (checkBalance(a)) {
+                balance = balance - a;
+                Console.WriteLine("$" + a + " deducted from credit card.");
+            } else {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
+
+        public bool checkBalance(double a) {
+            bool sufficient = false;
+
+            if (a > balance) {
+                sufficient = true;
+            }
+
+            return sufficient;
         }
     }
 }
