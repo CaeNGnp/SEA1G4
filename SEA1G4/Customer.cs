96bf4e024e169717ff7fe37f182179c32a1d89e9
Yuxuan Wong <s10196840@connect.np.edu.sg>
2022-02-08 02:43:11 +0800

    done ridedone state

---
+++ SEA1G4/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEA1G4 {
+    public class Customer : User, CustomerSubject {
+        private List<CustomerObserver> observers;
+        public double amountSpent;
+        public double points;
+        private CreditCard myCreditCard;
+
+        public Customer(string n, string c, string e, string id, double a, double p, CreditCard cc) : base(n, c, e, id) {
+            amountSpent = a;
+            points = p;
+            myCreditCard = cc;
+        }
+
+        public void payWithPoints() {
+            // to be implemented
+        }
+
+        public void payWithCreditCard(double amt) {
+            myCreditCard.deduct(amt);
+            amountSpent += amt;
+            Console.WriteLine("$" + amt + " deducted from credit card.");
+        }
+
+        public void addPoints(double amount) {
+            points += Convert.ToInt32(Math.Floor(amount));
+            Console.WriteLine(Convert.ToInt32(Math.Floor(amount)) + " PickUpNow points added");
+        }
+
+        public void notifyObservers() {
+            throw new NotImplementedException();
+        }
+
+        public void registerObserver(CustomerObserver co) {
+            throw new NotImplementedException();
+        }
+
+        public void removeObserver(CustomerObserver co) {
+            throw new NotImplementedException();
+        }
+    }
+}
