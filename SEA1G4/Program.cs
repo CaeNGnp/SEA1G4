d24d658a6ed7fd1450eed471da7e7e0f42d956a7
CaeNGnp <s10196427@connect.np.edu.sg>
2022-02-08 11:03:05 +0800

    upd

--- SEA1G4/Program.cs
+++ SEA1G4/Program.cs
@@ -39,7 +39,7 @@ namespace SEA1G4 {
             ride1.customer = c;
             ride1.driver = d2;
             // The current ride
-            Ride ride;
+            Ride ride = null;
             
             // hihi if u can move ur codes below this while loop its appreciated tq!
             // leaving here to avoid conflict
@@ -332,19 +332,18 @@ namespace SEA1G4 {
                     Rating r = new Rating(c, d1);
                 })
 
-                // Drivers option // aaaaa
+                // Drivers option
                 .AddHeading("Driver")
                 .AddOption("Accept booking", (m) => {
-                    while (true) {
-                        Console.WriteLine("Do you want to accept booking? [Y/N] ");
-
-                        string ans = Console.ReadLine().Trim().ToLower();
-                        if (ans == "y") {
-
-                        } else if (ans == "n") {
-                            continue;
-                        }
+                    Driver d = ride?.driver;
+                    if (d == null) {
+                        Console.WriteLine("No ride or assigned driver yet. Make a booking first");
+                        return;
                     }
+
+                    // need this?
+                    ride.rideCtx.changeState(new DriverAssignedState(ride.rideCtx));
+                    ride.rideCtx.acceptBooking();
                 })
                 .AddOption("Start ride", (m) => {
                     while (true) {
@@ -362,6 +361,7 @@ namespace SEA1G4 {
                 // Admins option
                 .AddHeading("Admin")
                 .AddOption("Process rating", (m) => {
+                    // probably not needed since it's an observer
                 })
                 .AddHeading()
                 .AddExitOption("Exit");
